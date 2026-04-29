7c97ecc0624f75cec7e1d6d234c1ed81bb88df84
Elle <elle@elle.codes>
2026-04-28 23:34:51 -0400

    Fixed selectors being english-dependent

--- firefox/background.js
+++ firefox/background.js
@@ -1,16 +1,20 @@
 function handleMessage(request, sender, sendResponse) {
     //sendResponse({ response: "Response from background script" });
     browser.storage.sync.get("options").then(function (data) {
+        if (!data.options) {
+            return;
+        }
+
         let css = data.options.css;
         if (data.options.layout === 'custom') {
             let composeWidth = data.options.composeWidth + data.options.composeUnits;
             let homeWidth = data.options.homeWidth + data.options.homeUnits;
             let notificationsWidth = data.options.notificationsWidth + data.options.notificationsUnits;
             let endWidth = data.options.endWidth + data.options.endUnits;
-            css = '.columns-area > .drawer { width: ' + composeWidth + ' !important; max-width: ' + composeWidth + ' !important; }'
-            + '.columns-area .column[aria-label="Home"] { width: ' + homeWidth + ' !important; max-width: ' + homeWidth + ' !important; }'
-            + '.columns-area .column[aria-label="Notifications"] { width: ' + notificationsWidth + ' !important; max-width: ' + notificationsWidth + ' !important; }'
-            + '.columns-area .column:last-child { width: ' + endWidth + ' !important; max-width: ' + endWidth + ' !important; }';
+            css = '.columns-area >.drawer:has(.compose-form) { width: ' + composeWidth + ' !important; max-width: ' + composeWidth + ' !important; }'
+            + '.columns-area .column:has(button .icon-home) { width: ' + homeWidth + ' !important; max-width: ' + homeWidth + ' !important; }'
+            + '.columns-area .column:has(button .icon-bell) { width: ' + notificationsWidth + ' !important; max-width: ' + notificationsWidth + ' !important; }'
+            + '.columns-area .column:has(button .icon-chevron-left) { width: ' + endWidth + ' !important; max-width: ' + endWidth + ' !important; }';
         }
 
         if (data.options.centered) {
